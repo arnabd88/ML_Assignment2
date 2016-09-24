401cae0ad81a95f57fb1dfd725dd962ae32a5458
arnabd <arnabd@lab1-19.eng.utah.edu>
2016-09-23 22:56:59 -0600

    Approximate solutions

--- Assignment2.py
+++ Assignment2.py
@@ -7,25 +7,33 @@ import math
 import numpy
 
 
-## -fold -test -margin 
+## -fold -test -margin -sanity
 
 fold_index = -1  # default required for single training file
 test_index = -1  # when testing pass  this switch
 foldValue = 0    # required with fold_index
 marginIndex = -1 ## default MarginList[0]; enables the CV with margins
 initRandom = 0   #  enable random init. Default zero
+sanityIndex = -1
 
 
 LRateList = [1, 0.1, 0.01]
 MarginList = [0,1,2,3,4,5]
 trainFileHandle = []
+sanityTable = []
+testFilehandle = []
 
 if ('-fold' in sys.argv):
 	fold_index = sys.argv.index('-fold')
 if('-test' in sys.argv):
 	test_index = sys.argv.index('-test')
+if('-sanity' in sys.argv):
+	sanityIndex = sys.argv.index('-sanity')
 
 
+
+if(sanityIndex != -1):
+	sanityTable.append(open( sys.argv[sanityIndex+1], 'r+').read().splitlines())
 if(fold_index != -1):
 	foldValue = int(sys.argv[fold_index + 1])
 	for f in range(1, int(sys.argv[fold_index + 1])+1):
@@ -35,9 +43,89 @@ else:
 	print 'Training files not provided .......... Exiting!!'
 	sys.exit()
 
-[XData, YData, FSize] = func.parseInfo(trainFileHandle)
-print max(FSize)
+if( test_index != -1 ):
+	testFilehandle.append(open( sys.argv[test_index+1], 'r+').read().splitlines())
+else:
+	print 'Test Data not Found..... No Testing!!'
+
+#[XData, YData, FSize] = func.parseInfo(trainFileHandle)
+#print YData, FSize
 
 
 
+##------- Run the simple perceptron from the table-2 data(Sanity Check) --------------
+def Run_Q3_3_1():
+	print "//Question-3.3.1 Report (Sanity check using Table2 -----------------------------"
+	[XData, YData, FSize] = func.parseInfo(sanityTable)
+	maxVecLen = max(FSize)
+	for i in range(1,11):
+		for j in LRateList:
+			[Bias, Wvec, LearningMistake] = func.SimplePerceptron(j, 0, XData, YData, maxVecLen, i, 1)
+			mistakeCount = func.TestSimplePerceptron(XData, YData, Wvec, Bias, maxVecLen)
+			TrainAccuracy = (float(len(XData) - mistakeCount)/len(XData))*100
+			print 'Using Learning Rate = ',j,' number of epochs = ',i, 'Initialization = @ default 0'
+			print '\t Learned Weight Vector    = ', Wvec
+			print '\t Learned Bias             = ', Bias
+			print '\t Mistakes during Learning = ', LearningMistake
+			print '\t Learning Accuracy        = ', TrainAccuracy,'%\n'
+	print "//End of Question-3.3.1 Report -------------------------------------------------\n\n"
+
+
+##------- Run the simple perceptron on adult data for training and testing ------##
+def Run_Q3_3_2_sp():
+	print "//Question-3.3.2 (Simple Perceptron) Report on Adult Data -----------------------------"
+	[XData, YData, FSize] = func.parseInfo(trainFileHandle)
+	maxVecLen = max(FSize)
+	for i in range(1,5):
+		for j in LRateList:
+			[Bias, Wvec, LearningMistake] = func.SimplePerceptron(j, 1, XData, YData, maxVecLen, i, 1)
+			mistakeCount = func.TestSimplePerceptron(XData, YData, Wvec, Bias, maxVecLen)
+			TrainAccuracy = ((float(len(XData) - mistakeCount))/len(XData))*100
+			print 'Using Learning Rate = ',j,' number of epochs = ',i, 'Initialization = random with mean 0 and sd 0.1'
+		#	print '\t Learned Weight Vector    = ', Wvec
+			print '\t Learned Bias             = ', Bias
+			print '\t Mistakes during Learning = ', LearningMistake
+			print '\t Learning Accuracy        = ', TrainAccuracy,'%\n'
+			if(test_index != -1):
+				[testXData, testYData] = func.parseInfoTest(testFilehandle, maxVecLen)
+				testMistake = func.TestSimplePerceptron(testXData, testYData, Wvec, Bias, maxVecLen)
+				TestAccuracy = ((float(len(testXData) - testMistake))/len(testXData))*100
+				print '\t Test Accuracy            = ', TestAccuracy,'%\n'
+	print "//End of Question-3.3.2 Report ------------------------------------\n\n"
+
+
+
+##-------- Run the margin perceptron on adult data for training and testing ----##
+def Run_Q3_3_2_mp():
+	print "//Question-3.3.2 (Margin Perceptron) Report on Adult Data -----------------------------"
+	[XData, YData, FSize] = func.parseInfo(trainFileHandle)
+	maxVecLen = max(FSize)
+	for i in range(1,5):
+		for j in LRateList:
+			for k in MarginList:
+				[Bias, Wvec, LearningMistake] = func.MarginPerceptron(j, 1, XData, YData, maxVecLen, i, k, 1)
+				mistakeCount = func.TestMarginPerceptron(XData, YData, Wvec, Bias, maxVecLen, k)
+				TrainAccuracy = ((float(len(XData) - mistakeCount))/len(XData))*100
+				print 'Using Learning Rate = ',j,', number of epochs = ',i,', Margin = ', k,  ', Initialization = random with mean 0 and sd 0.1'
+			#	print '\t Learned Weight Vector    = ', Wvec
+				print '\t Learned Bias             = ', Bias
+				print '\t Mistakes during Learning = ', LearningMistake
+				print '\t Learning Accuracy        = ', TrainAccuracy,'%\n'
+				if(test_index != -1):
+					[testXData, testYData] = func.parseInfoTest(testFilehandle, maxVecLen)
+					testMistake = func.TestMarginPerceptron(testXData, testYData, Wvec, Bias, maxVecLen, k)
+					TestAccuracy = ((float(len(testXData) - testMistake))/len(testXData))*100
+					print '\t Test Accuracy            = ', TestAccuracy,'%\n'
+	print "//End of Question-3.3.2 Report ------------------------------------\n\n"
+
+
+
+
+
+
+
+
+Run_Q3_3_1()
+Run_Q3_3_2_sp()
+Run_Q3_3_2_mp()
 
--- func.py
+++ func.py
@@ -35,11 +35,34 @@ def trimList( list1 ):
 			l2.append(i)
 	return l2
 
+
+def sgn( value, margin ):
+	if( value >= margin):
+		return 1
+	else:
+		return -1
+
 def extendList( l1, maxSize ):
 	size = maxSize - len(l1)
 	newL = [0]*size
 	l1.extend(newL)
 	return l1
+
+
+def permuteDataLabel( xdata, ylabel ):
+	newdata = []
+	newlabel = []
+	shufIdx = numpy.random.permutation(len(xdata))
+	if( len(xdata) != len(ylabel)):
+		print 'Error......... Mismatch in data and label size..... Exiting'
+		sys.exit()
+
+	for i in shufIdx:
+		newdata.append(xdata[i])
+		newlabel.append(ylabel[i])
+
+	return [newdata, newlabel]
+	
 
 
 def parseInfo( rawData ):
@@ -56,12 +79,146 @@ def parseInfo( rawData ):
 			x_temp = [0]*currMax
 			for i in lineList[1:]:
 				arg = i.split(':')
-				if(int(arg[0]) > len(x_temp)):
-					x_temp = extendList(x_temp, int(arg[0]))
-				x_temp[int(arg[0])-1] = int(arg[1])
+				if(int(arg[0])+1 > len(x_temp)):
+					x_temp = extendList(x_temp, int(arg[0])+1)
+				x_temp[int(arg[0])] = int(arg[1])
 			XData.append(x_temp)
 			Fsize.append(len(x_temp))
 	return [XData, YData, Fsize]
-				
+
+def parseInfoTest( testData, maxSize ):
+	XData  = []
+	YData  = []
+	Fsize  = []
+	x_temp = []
+
+	for flist in testData:
+		for line in flist:
+			lineList = line.split()
+			YData.append(int(lineList[0]))
+			x_temp = [0]*maxSize
+			for i in lineList[1:]:
+				arg = i.split(':')
+				if(int(arg[0]) < maxSize):
+					x_temp[int(arg[0])] = int(arg[1])
+			XData.append(x_temp)
+	return [XData, YData]
+
+
+def MarginPerceptron(lr, initEn, xdata, ydata, wsize, epochs, margin, enShuffle ):
+	wvec = []
+	bias = 0
+	mistakeCounter = 0
+	##---initialize---
+	if(initEn==0):
+		wvec = [0]*wsize
+		bias = 0
+	else:
+		##--- using a normal distribution of mean 0 and sd 0.1
+		wvec = numpy.random.normal(0, 0.01, wsize+1)
+		bias = wvec[0]
+		wvec = wvec[1:]
+	##-- Make the prediction (Evaluate wx+b) -- ##
+	for i in range(0,epochs):
+		wtxSum = bias
+		looprange = len(wvec)
+		if(enShuffle == 1):
+			[xdata, ydata] = permuteDataLabel(xdata, ydata)
+		for i in range(0,len(xdata)):  ## looping over each of the examples
+			xvec = xdata[i]
+			ylabel = ydata[i]
+			if(len(xvec) < len(wvec)):
+				looprange = len(xvec)
+			for i in range(0,looprange):
+				wtxSum = wtxSum + xvec[i]*wvec[i]
+			if(sgn(wtxSum, margin)*ylabel < 0): ## Incorrect prediction 
+				mistakeCounter = mistakeCounter + 1
+				## update the bias and weights
+				if(len(xvec) < len(wvec)):
+					xvec = extendList(xvec,len(wvec))
+				correction = map(lambda x: x*ylabel*lr, xvec)
+				wvec = map(sum, zip(wvec, correction))
+				bias = bias + lr*ylabel
+	return [bias, wvec, mistakeCounter]
+
+
+## lr      = learning rate, initEn = randomInitialization Enabled
+## xdata   = set of training examples [[],[],...]
+## ydata   = set of labels of the training examples [,,,...]
+## setBias = may be added, not decided
+## wsize   = size of the weight vector
+## epochs  = number of epochs  required
+
+def SimplePerceptron(lr, initEn, xdata, ydata, wsize, epochs, enShuffle ):
+	wvec = []
+	bias = 0
+	mistakeCounter = 0
+	##---initialise ---
+	if(initEn == 0): ## set bias and weight to zero
+		wvec = [0]*wsize
+		bias = 0
+	else:
+		##--- using a normal distribution of mean 0 and sd 0.1
+		wvec = numpy.random.normal(0, 0.01, wsize+1)
+		bias = wvec[0]
+		wvec = wvec[1:]
+	##-- Make the prediction (Evaluate wx+b) -- ##
+	for i in range(0,epochs):
+		wtxSum = bias
+		looprange = len(wvec)
+		if(enShuffle == 1):
+			[xdata, ydata] = permuteDataLabel(xdata, ydata)
+		for i in range(0,len(xdata)):  ## looping over each of the examples
+			xvec = xdata[i]
+			ylabel = ydata[i]
+			if(len(xvec) < len(wvec)):
+				looprange = len(xvec)
+			for i in range(0,looprange):
+				wtxSum = wtxSum + xvec[i]*wvec[i]
+			if(sgn(wtxSum, 0)*ylabel < 0): ## Incorrect prediction 
+				mistakeCounter = mistakeCounter + 1
+				## update the bias and weights
+				if(len(xvec) < len(wvec)):
+					xvec = extendList(xvec,len(wvec))
+				correction = map(lambda x: x*ylabel*lr, xvec)
+				wvec = map(sum, zip(wvec, correction))
+				bias = bias + lr*ylabel
+	return [bias, wvec, mistakeCounter]
+
+## Takes test data and makes predictions based on the 
+
+def TestSimplePerceptron(xdata, ydata, wvec, bias, wsize):
+	mistakeCounter = 0
+	wtxSum = bias
+	looprange = len(wvec)
+	for i in range(0,len(xdata)):
+		xvec   = xdata[i]
+		ylabel = ydata[i]
+		if(len(xvec) < len(wvec)):
+			looprange = len(xvec)
+		for i in range(0,looprange):
+			wtxSum = wtxSum + xvec[i]*wvec[i]
+		if(sgn(wtxSum, 0)*ylabel < 0):
+			mistakeCounter = mistakeCounter + 1
+	return mistakeCounter
+	
+
+def TestMarginPerceptron(xdata, ydata, wvec, bias, wsize, margin):
+	mistakeCounter = 0
+	wtxSum = bias
+	looprange = len(wvec)
+	for i in range(0,len(xdata)):
+		xvec   = xdata[i]
+		ylabel = ydata[i]
+		if(len(xvec) < len(wvec)):
+			looprange = len(xvec)
+		for i in range(0,looprange):
+			wtxSum = wtxSum + xvec[i]*wvec[i]
+		if(sgn(wtxSum, margin)*ylabel < 0):
+			mistakeCounter = mistakeCounter + 1
+	return mistakeCounter
+	
+	
+
 		
 
